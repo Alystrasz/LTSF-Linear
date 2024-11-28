119c52fce47fc70d41f4a51a5648bbf807f2e361
Remy Raes <contact@remyraes.com>
2024-11-28 16:24:03 +0100

    build: add custom script for compression experiments

---
+++ scripts/EXP-LongForecasting/Linear/univariate/ettm1_compression.sh
@@ -0,0 +1,25 @@
+if [ ! -d "./logs" ]; then
+    mkdir ./logs
+fi
+
+if [ ! -d "./logs/LongForecasting" ]; then
+    mkdir ./logs/LongForecasting
+fi
+
+if [ ! -d "./logs/LongForecasting/univariate" ]; then
+    mkdir ./logs/LongForecasting/univariate
+fi
+model_name=NLinear
+
+python -u run_longExp.py \
+  --is_training 1 \
+  --root_path ./dataset/ \
+  --data_path ETTm1.csv \
+  --model_id ETTm1_336_720 \
+  --model $model_name \
+  --data ETTm1 \
+  --seq_len 336 \
+  --pred_len 720 \
+  --enc_in 1 \
+  --des 'Exp' \
+  --itr 1 --batch_size 8 --learning_rate 0.0001 --feature S >logs/LongForecasting/$model_name'_'fS_ETTm1_336_720.log
