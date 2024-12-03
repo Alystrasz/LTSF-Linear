46fab9d428b5a6b8d02ca2ce09fddf68759c247f
Remy Raes <contact@remyraes.com>
2024-12-03 11:25:00 +0100

    feat: reimplement __read_data__ method with debug print calls

--- data_provider/data_loader.py
+++ data_provider/data_loader.py
@@ -193,10 +193,63 @@ class Dataset_Compressed_ETT_minute(Dataset_ETT_minute):
                  features='S', data_path='ETTm1.csv',
                  target='OT', scale=True, timeenc=0, freq='t', train_only=False,
                  preserve_ratio=1):
-        super().__init__(root_path, flag, size, features, data_path, target, scale, timeenc, freq, train_only)
+
+        # Save compression variables
         self.preserve_ratio = preserve_ratio
-        if preserve_ratio > 1:
-            print("Dataset will need compression!")
+
+        super().__init__(root_path, flag, size, features, data_path, target, scale, timeenc, freq, train_only)
+
+    def __read_data__(self):
+        self.scaler = StandardScaler()
+        df_raw = pd.read_csv(os.path.join(self.root_path,
+                                          self.data_path))
+
+        border1s = [0, 12 * 30 * 24 * 4 - self.seq_len, 12 * 30 * 24 * 4 + 4 * 30 * 24 * 4 - self.seq_len]
+        border2s = [12 * 30 * 24 * 4, 12 * 30 * 24 * 4 + 4 * 30 * 24 * 4, 12 * 30 * 24 * 4 + 8 * 30 * 24 * 4]
+        border1 = border1s[self.set_type]
+        border2 = border2s[self.set_type]
+
+        if self.preserve_ratio > 1:
+            print(f"border1s: {border1s}")
+            print(f"border2s: {border2s}")
+            print(f"border1: {border1}")
+            print(f"border2: {border2}")
+
+        if self.features == 'M' or self.features == 'MS':
+            cols_data = df_raw.columns[1:]
+            df_data = df_raw[cols_data]
+        elif self.features == 'S':
+            df_data = df_raw[[self.target]]
+
+        if self.scale:
+            train_data = df_data[border1s[0]:border2s[0]]
+            self.scaler.fit(train_data.values)
+            data = self.scaler.transform(df_data.values)
+        else:
+            data = df_data.values
+
+        df_stamp = df_raw[['date']][border1:border2]
+        df_stamp['date'] = pd.to_datetime(df_stamp.date)
+
+        print(f"self.timeenc: {self.timeenc}")
+        if self.timeenc == 0:
+            df_stamp['month'] = df_stamp.date.apply(lambda row: row.month, 1)
+            df_stamp['day'] = df_stamp.date.apply(lambda row: row.day, 1)
+            df_stamp['weekday'] = df_stamp.date.apply(lambda row: row.weekday(), 1)
+            df_stamp['hour'] = df_stamp.date.apply(lambda row: row.hour, 1)
+            df_stamp['minute'] = df_stamp.date.apply(lambda row: row.minute, 1)
+            df_stamp['minute'] = df_stamp.minute.map(lambda x: x // 15)
+            data_stamp = df_stamp.drop(['date'], 1).values
+        elif self.timeenc == 1:
+            data_stamp = time_features(pd.to_datetime(df_stamp['date'].values), freq=self.freq)
+            data_stamp = data_stamp.transpose(1, 0)
+
+        self.data_x = data[border1:border2]
+        self.data_y = data[border1:border2]
+        self.data_stamp = data_stamp
+
+        print(f"len(data_x): {len(self.data_x)}")
+        print(f"len(data_y): {len(self.data_y)}")
 
 class Dataset_Custom(Dataset):
     def __init__(self, root_path, flag='train', size=None,
